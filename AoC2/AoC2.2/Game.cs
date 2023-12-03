126c9acc572a02d1750e0acb86bb3895d0d63ba7
l1chorpe <44500002+l1chorpe@users.noreply.github.com>
2023-12-03 01:24:20 +0100

    Added code for part 2 of the challenge

---
+++ AoC2/AoC2.2/Game.cs
@@ -0,0 +1,85 @@
+﻿namespace AoC2;
+
+public class Game
+{
+    private class Subset
+    {
+        private const int LEGAL_RED = 12, LEGAL_GREEN = 13, LEGAL_BLUE = 14;
+        public readonly int Red, Green, Blue;
+
+        public Subset((int, int, int) rgb) { (Red, Green, Blue) = rgb; }
+        public bool IsLegal() { return Red <= LEGAL_RED && Green <= LEGAL_GREEN && Blue <= LEGAL_BLUE; }
+    }
+
+    public readonly int ID;
+    private List<Subset> Subsets { get; }
+
+    public Game(string idAndSubsets)
+    {
+        idAndSubsets = idAndSubsets.Replace(' ', '\0');
+        var split = idAndSubsets.Split(':');
+        ID = GetNumber(split[0]);
+        Subsets = GetSubsets(split[1]);
+    }
+
+    /// <summary>
+    /// Retrieves the number from the given string.
+    /// </summary>
+    /// <returns>The number formatted to int</returns>
+    private static int GetNumber(string id)
+    {
+        string result = "";
+        foreach(char c in id)
+            if(char.IsDigit(c))
+                result += c;
+        return int.Parse(result);
+    }
+
+    /// <summary>
+    /// Converts the given string into a list of subsets.
+    /// </summary>
+    private static List<Subset> GetSubsets(string subsets)
+    {
+        // It's just basic string manipulation, nothing too wild
+        List<Subset> result = new();
+        foreach(string subset in subsets.Split(';'))
+        {
+            (int, int, int) rgb = (0, 0, 0);
+            foreach(string color_cube in subset.Split(','))
+            {
+                int cube_count = GetNumber(color_cube);
+                if(color_cube.Contains("red"))
+                    rgb.Item1 = cube_count;
+                else if(color_cube.Contains("green"))
+                    rgb.Item2 = cube_count;
+                else
+                    rgb.Item3 = cube_count;
+            }
+            result.Add(new(rgb));
+        }
+        return result;
+    }
+
+    public bool IsLegal()
+    {
+        foreach(Subset subset in Subsets)
+            if(!subset.IsLegal())
+                return false;
+        return true;
+    }
+
+    public int GetPower()
+    {
+        int red = 0, green = 0, blue = 0;
+        foreach (Subset subset in Subsets)
+        {
+            if (subset.Red > red)
+                red = subset.Red;
+            if (subset.Green > green)
+                green = subset.Green;
+            if (subset.Blue > blue)
+                blue = subset.Blue;
+        }
+        return red * green * blue;
+    }
+}
---
+++ AoC2/AoC2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AoC2;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        List<Game> list = new();
+        foreach(string line in File.ReadAllLines("data.txt"))
+            list.Add(new Game(line.Trim()));
+
+        int power_sum = 0;
+        foreach (Game game in list)
+            power_sum += game.GetPower();
+
+        Console.WriteLine(power_sum);
+    }
+}
