5337b6d37eb1cd4eb512c5cad6431976a4dd8161
l1chorpe <44500002+l1chorpe@users.noreply.github.com>
2023-12-03 01:03:47 +0100

    Added code for part 1 of the challenge

---
+++ AoC2/AoC2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AoC2;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        List<Game> list = new();
+        foreach(string line in File.ReadAllLines("data.txt"))
+            list.Add(new Game(line.Trim()));
+
+        int sum = 0;
+        foreach (Game game in list)
+            if (game.IsLegal())
+                sum += game.ID;
+
+        Console.WriteLine(sum);
+    }
+}
