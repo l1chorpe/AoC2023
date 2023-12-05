4e0800a3ec3f923298e25d791befd347afbfad24
l1chorpe <s.feliciani.141@gmail.com>
2023-12-05 05:34:46 +0100

    Added code for part 2 of day 3

---
+++ AoC3/AoC3.2/Line.cs
@@ -0,0 +1,49 @@
+﻿namespace AoC3;
+
+public class Line
+{
+    public readonly List<Number> Numbers = new();
+    public readonly List<Symbol> Symbols = new();
+
+    public Line(string line)
+    {
+        string buffer = "";
+        for (int i = 0; i < line.Length; ++i)
+        {
+            // Add every digit to the buffer
+            if (char.IsDigit(line[i]))
+            {
+                buffer += line[i];
+            }
+            else
+            {
+                // If the current char is not a digit, add the number in the buffer (if necessary) and reset the buffer,
+                // and add a new symbol to the list if the char is not '.'
+                if (buffer != "")
+                {
+                    Numbers.Add(new(i - buffer.Length, i - 1, int.Parse(buffer)));
+                    buffer = "";
+                }
+                if (line[i] != '.')
+                    Symbols.Add(new(i, line[i]));
+            }
+        }
+        if(buffer != "")
+            Numbers.Add(new(line.Length - buffer.Length, line.Length - 1, int.Parse(buffer)));
+    }
+}
+
+public class Number
+{
+    public readonly int StartPos, EndPos, Value;
+
+    public Number(int startPos, int endPos, int value) { StartPos = startPos; EndPos = endPos; Value = value; }
+}
+
+public class Symbol
+{
+    public readonly int Pos;
+    public readonly char Character;
+
+    public Symbol(int pos, char character) { Pos = pos; Character = character; }
+}
---
+++ AoC3/AoC3.2/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace AoC3;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        // Parse all the lines
+        List<Line> lines = new();
+        foreach (string line in File.ReadAllLines("data.txt"))
+        {
+            lines.Add(new Line(line));
+        }
+
+        // Sum all the lines' gear ratios
+        int sum = 0;
+        for (int i = 0; i < lines.Count; ++i)
+            sum += GetGearRatio(
+                i == 0 ? null : lines[i - 1],
+                lines[i],
+                i == lines.Count - 1 ? null : lines[i + 1]
+            );
+
+        Console.WriteLine(sum);
+    }
+
+    /// <summary>
+    /// Finds all the '*' adjacent to exactly two numbers (if there are any).
+    /// </summary>
+    /// <returns>The sum of all gear ratios</returns>
+    private static int GetGearRatio(in Line? previous_line, in Line current_line, in Line? next_line)
+    {
+        int ratio_sum = 0;
+        foreach (Symbol symbol in current_line.Symbols)
+        {
+            if (symbol.Character == '*')
+            {
+                List<Number> adjacent_numbers = new();
+
+                // Go through all numbers on the three lines and find those that are close enough to the current '*'
+                if (previous_line != null)
+                    foreach (Number number in previous_line.Numbers)
+                        if (symbol.Pos >= number.StartPos - 1 && symbol.Pos <= number.EndPos + 1)
+                            adjacent_numbers.Add(number);
+
+                if (next_line != null)
+                    foreach (Number number in next_line.Numbers)
+                        if (symbol.Pos >= number.StartPos - 1 && symbol.Pos <= number.EndPos + 1)
+                            adjacent_numbers.Add(number);
+
+                foreach (Number number in current_line.Numbers)
+                    if (symbol.Pos >= number.StartPos - 1 && symbol.Pos <= number.EndPos + 1)
+                        adjacent_numbers.Add(number);
+
+                // Add the gear ratio if all goes well
+                if (adjacent_numbers.Count == 2)
+                    ratio_sum += adjacent_numbers[0].Value * adjacent_numbers[1].Value;
+            }
+        }
+        return ratio_sum;
+    }
+}
